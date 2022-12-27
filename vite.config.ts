1eb7d98897556a54e103b56ba0247486e884798e
Danu Widatama <widatama@gmail.com>
2022-12-27 19:01:03 +0700

    Set up html and pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,21 @@ import { resolve } from 'path';
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 
 const outDir = resolve(__dirname, 'dist');
 const rootDir = resolve(__dirname, 'src');
 const publicDir = resolve(__dirname, 'public');
+
+const appTitle = 'Angkot';
+
+const inject = {
+  data: {
+    appTitle,
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,7 +32,46 @@ export default defineConfig({
     },
   },
   publicDir,
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({ inject }),
+    VitePWA({
+      devOptions: { enabled: true },
+      injectRegister: 'inline',
+      registerType: 'autoUpdate',
+      manifest: {
+        name: appTitle,
+        short_name: appTitle,
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#333333',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
